bf3b9ea8c256a5a1663ee6cb5af9fced0360e448
tianqing <tianqing@lxzwedu.com>
2024-12-17 15:04:03 +0800

    feat: 搭建

---
+++ src/views/myHome/myHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>这是我的</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
